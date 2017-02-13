013666bdd098f3bdde7f55eedb081c0f76114285
mpotapova-vmw <mpotapova@vmware.com>
2017-02-13 13:49:17 -0800

    additional plumbing of scopes, lookup, etc. needed for demo (#5)
    * additional plumbing of scopes, lookup, etc. needed for demo
    
    * wrapping scopes logging into if debug

--- server/src/main/java/org/cloudfoundry/identity/uaa/scim/jdbc/VmidentityScimUserProvisioning.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/scim/jdbc/VmidentityScimUserProvisioning.java
@@ -12,9 +12,12 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.scim.jdbc;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.cloudfoundry.identity.uaa.constants.OriginKeys;
 import org.cloudfoundry.identity.uaa.resources.ResourceMonitor;
 import org.cloudfoundry.identity.uaa.scim.ScimMeta;
@@ -26,14 +29,19 @@ import org.cloudfoundry.identity.uaa.scim.exception.InvalidScimResourceException
 import org.cloudfoundry.identity.uaa.scim.exception.ScimResourceNotFoundException;
 import org.cloudfoundry.identity.uaa.util.VmidentityUtils;
 
+import com.unboundid.scim.sdk.SCIMException;
+import com.unboundid.scim.sdk.SCIMFilter;
+import com.unboundid.scim.sdk.SCIMFilterType;
 import com.vmware.identity.idm.InvalidPrincipalException;
 import com.vmware.identity.idm.PersonDetail;
 import com.vmware.identity.idm.PersonUser;
 import com.vmware.identity.idm.PrincipalId;
 import com.vmware.identity.idm.client.CasIdmClient;
 
+
 public class VmidentityScimUserProvisioning implements ScimUserProvisioning, ResourceMonitor<ScimUser> {
 
+    private final Log logger = LogFactory.getLog(VmidentityScimUserProvisioning.class);
     private final CasIdmClient _idmClient;
 
     public VmidentityScimUserProvisioning(CasIdmClient casIdmClient) {
@@ -96,13 +104,102 @@ public class VmidentityScimUserProvisioning implements ScimUserProvisioning, Res
     }
 
     @Override
-    public List<ScimUser> query(String filter) {
-        throw new UnsupportedOperationException();
+    public List<ScimUser> query(String filter)
+    {
+        logger.debug(String.format("query called with filter '%s'", filter));
+
+        ArrayList<ScimUser> users = new ArrayList<ScimUser>();
+        SCIMFilter scimFilter = null;
+        try
+        {
+            scimFilter = this.scimFilter(filter);
+        }
+        catch (SCIMException ex)
+        {
+            logger.debug(String.format("Invalid scim filter '%s'", filter), ex);
+            throw new IllegalStateException(String.format("Invalid scim filter '%s'", filter), ex);
+        }
+
+        // todo: only support simple filters for now
+        // we need implement proper filter support
+        //id eq "#{user_id}" or id eq "#{user_id}" or ...
+        //username eq "#{username}"
+
+        // UserIdConversionEndpoints does a wrapping of this filter
+        // with an AND (origin 1 or origin 2 or origin3 ...) from all active providers
+        // idm should always only return active providers.
+        //
+        // todo: we should revisit if we need existing logic in UserIdConversionEndpoints
+        // on this layer it might be unsafe to ignore origin should it ever be done by other logic
+        // does not seem to be like this atm ...
+
+        List<SCIMFilter> scimFilters = null;
+        if ( ( scimFilter.getFilterType() == SCIMFilterType.AND )
+             &&
+             (scimFilter.getFilterComponents().size() == 2) )
+        {
+            if ( isOriginFilter( scimFilter.getFilterComponents().get(1) ) )
+            {
+                scimFilter = scimFilter.getFilterComponents().get(0);
+            }
+            else if ( isOriginFilter( scimFilter.getFilterComponents().get(0) ) )
+            {
+                scimFilter = scimFilter.getFilterComponents().get(1);
+            }
+        }
+
+        if ( scimFilter.getFilterType() == SCIMFilterType.OR )
+        {
+            scimFilters = scimFilter.getFilterComponents();
+        }
+        else if ( scimFilter.getFilterType() == SCIMFilterType.EQUALITY )
+        {
+            scimFilters = new ArrayList<SCIMFilter>();
+            scimFilters.add(scimFilter);
+        }
+        else
+        {
+            logger.debug(String.format("Scim filter '%s' not yet supported", filter));
+            throw new UnsupportedOperationException(String.format("Scim filter '%s' not yet supported", filter));
+        }
+
+        for(SCIMFilter f : scimFilters)
+        {
+            // for now only support equality by id or username
+            if ( f.getFilterType() == SCIMFilterType.EQUALITY )
+            {
+                if ( ( "id".equalsIgnoreCase(f.getFilterAttribute().getAttributeName()) )
+                    || "username".equalsIgnoreCase(f.getFilterAttribute().getAttributeName()))
+                {
+                    if ( f.getFilterValue() != null )
+                    {
+                        users.add(this.retrieve(f.getFilterValue()));
+                    }
+                }
+                else
+                {
+                    logger.debug(String.format("Scim filter '%s' attribute '%s' not yet supported",
+                            filter, f.getFilterAttribute().getAttributeName()));
+                    throw new UnsupportedOperationException(
+                        String.format("Scim filter '%s' attribute '%s' not yet supported",
+                            filter, f.getFilterAttribute().getAttributeName()));
+
+                }
+            }
+            else
+            {
+                logger.debug(String.format("Scim filter '%s' not yet supported", filter));
+                throw new UnsupportedOperationException(String.format("Scim filter '%s' not yet supported" , filter));
+            }
+        }
+        return users;
     }
 
     @Override
-    public List<ScimUser> query(String filter, String sortBy, boolean ascending) {
-        throw new UnsupportedOperationException();
+    public List<ScimUser> query(String filter, String sortBy, boolean ascending)
+    {
+        // todo: sort by, ascending
+        return this.query(filter);
     }
 
     @Override
@@ -193,5 +290,48 @@ public class VmidentityScimUserProvisioning implements ScimUserProvisioning, Res
         // user.setSalt(salt);
         user.setPasswordLastModified(new Date(personUser.getDetail().getPwdLastSet()));
         return user;
+    }
+
+    private SCIMFilter scimFilter(String filter) throws SCIMException {
+        SCIMFilter scimFilter;
+        try {
+            scimFilter = SCIMFilter.parse(filter);
+        } catch (SCIMException e) {
+            logger.debug("Attempting legacy scim filter conversion for [" + filter + "]", e);
+            filter = filter.replaceAll("'","\"");
+            scimFilter = SCIMFilter.parse(filter);
+        }
+        return scimFilter;
+    }
+
+    private static boolean isOriginEqualityFilter(SCIMFilter filter)
+    {
+        return
+           ( (filter.getFilterType() == SCIMFilterType.EQUALITY)
+            &&
+             ("origin".equalsIgnoreCase(filter.getFilterAttribute().getAttributeName()) ) );
+    }
+
+    private static boolean isOriginFilter(SCIMFilter filter)
+    {
+        boolean result = false;
+        if (isOriginEqualityFilter(filter))
+        {
+            result = true;
+        }
+        else if ( filter.getFilterType() == SCIMFilterType.OR )
+        {
+            result = true;
+            for(SCIMFilter component : filter.getFilterComponents() )
+            {
+                if ( isOriginEqualityFilter(component) == false )
+                {
+                    result = false;
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 }
