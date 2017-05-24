64d6599611403d2b633d62acbeab47f27cc0bf70
Travis Hall <thall@vmware.com>
2017-05-23 20:39:01 -0700

    Added a passthrough password encoder
    This allows the underlying infrastructure to perform the password
    encoding and just performs a string comparison for the passwords. This
    was necessary in order to allow clients to be created through multiple
    mechanisms in VMidentity (e.g. REST IDM or UAA).
    
    One unfortunate side-effect of this change is that the password encoding
    is inherently reversible, but this is already the case and regularly
    used for Identity Source passwords when stored in VMDir.

---
+++ server/src/main/java/org/cloudfoundry/identity/uaa/util/PassthroughPasswordEncoder.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ *     Cloud Foundry
+ *     Copyright (c) [2017] VMware, Inc. All Rights Reserved.
+ *
+ *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ *     You may not use this product except in compliance with the License.
+ *
+ *     This product includes a number of subcomponents with
+ *     separate copyright notices and license terms. Your use of these
+ *     subcomponents is subject to the terms and conditions of the
+ *     subcomponent's license, as noted in the LICENSE file.
+ *******************************************************************************/
+package org.cloudfoundry.identity.uaa.util;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Simple passthrough password encoder that lets the underlying storage
+ * mechanism handle encoding/decoding.
+ */
+public class PassthroughPasswordEncoder implements PasswordEncoder {
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return rawPassword.toString();
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encodedPassword.equals(rawPassword.toString());
+    }
+}
